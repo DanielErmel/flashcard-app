4244724f1f499464668636ef6a6a63567be41abe
Daniel Ermel <110058804+DanielErmel@users.noreply.github.com>
2022-10-11 10:27:36 -0500

    Add files via upload

---
+++ src/Layout/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ListDecks from "../Deck/DeckList";
+import {Link} from "react-router-dom";
+
+function Home({decks, setDecks}) {
+  return (
+    
+    <div>
+      <div>
+        <Link to="/decks/new"><button className="btn btn-primary btn-large"><i className="bi bi-plus"></i>Create Deck</button></Link>
+      </div>
+      <ListDecks decks={decks} setDecks={setDecks} />
+    </div>
+    
+  )
+}
+
+export default Home;
